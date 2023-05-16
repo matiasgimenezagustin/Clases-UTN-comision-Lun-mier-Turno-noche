c648864736a13fb12173169952e4a88d1168cc90
matu.dev <matu.a.gimenez2002@gmail.com>
2023-05-16 14:24:29 -0300

    clase 1 JS

---
+++ js-clase-1/script.js
@@ -0,0 +1,29 @@
+/* 
+Que es un lenguaje de programacion?
+
+Para quien esta hecho el lenguaje de programacion?
+
+
+Javascript es un lenguaje de programacion interpretado (compilado just in time (en el momento)), de alto nivel, debilmente tipado
+*/
+
+//Tipos de datos primitivos
+
+//Numbers
+90
+-90
+6.4
+Infinity
+NaN
+//Strings = cadena de texto ''  ""
+'Menganito es un buen alumno'
+'8'
+//Boolean
+true
+false
+//Undefined
+undefined
+//Null
+null
+
+/* console.log('hola') */
